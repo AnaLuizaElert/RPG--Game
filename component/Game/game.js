772acd63daadc50f24463846ef6386e4d41672d1
AnaLuizaElert <ana_elert@estudante.sesisenai.org.br>
2023-04-25 11:16:35 -0300

    error Pressable

--- component/Game/game.js
+++ component/Game/game.js
@@ -10,47 +10,62 @@ import {
   ScrollView} 
   from 'react-native';
 
-
-function choose1(){
-  history = "asdasd";
-  option1 = "asdas";
-  option2 = "sdfcasdf";
-  // onRefresh;
-}
-
-const [refreshing, setRefreshing] = React.useState(false);
-
-const onRefresh = React.useCallback(() => {
-  setRefreshing(true);
-  setTimeout(() => {
-    setRefreshing(false);
-  }, 2000);
-}, []);
-
-const image = {uri: 'https://templatefor.net/wp-content/uploads/2018/04/iPhone-forest-mysterious-wallpaper-245619875-576x1024.jpg'};
-let history = `Consigo abrir meus olhos, mas não enxergo nada, nada que eu consiga entender. Quem são vocês? Por que me olham? O que querem? 
-Percebo que estou na antiga cabana de meus falecidos pais, os estranhos saem do quarto, e eu fico deitada. Vejo a janela aberta que me chama em direção a floresta, mas a curiosidade me chama a escutar o que falam…`;
-let option1 = "Escuto a conversa";
-let option2 = "Pulo a janela";
-
-
 export default function Game(){
+  const image = {uri: 'https://templatefor.net/wp-content/uploads/2018/04/iPhone-forest-mysterious-wallpaper-245619875-576x1024.jpg'};
+  const [history, setHistory] = React.useState(`Consigo abrir meus olhos, mas não enxergo nada, nada que eu consiga entender. Quem são vocês? Por que me olham? O que querem? 
+  Percebo que estou na antiga cabana de meus falecidos pais, os estranhos saem do quarto, e eu fico deitada. Vejo a janela aberta que me chama em direção a floresta, mas a curiosidade me chama a escutar o que falam…`);
+  const [option1, setOption1] = React.useState("Escuto a conversa");
+  const [option2, setOption2] = React.useState("Pulo a janela");
+
+  function choose1(){
+    setHistory(`Me levanto e coloco meus ouvidos sobre a porta, ela está velha, há muitos cupins nela, eu quebro a parte superior da porta, fazendo um barulho relativamente alto. Portanto, escuto
+    – “Nós não temos muitas opções restando, ela não vai colaborar e vcs já estão percebendo!”
+    Em seguida ouço passos em direção ao meu quarto e a dúvida lateja em minha cabeça`);
+    setOption1("Me deito");
+    setOption2("Pulo a janela");
+    Pressable.call(choose11())
+  }
+
+  function choose2(){
+    setHistory(`Rapidamente me levanto e lembrando de quando eu brincava com meu irmão, pulo com cuidado a janela. Sei por onde ir, é um instinto construído que aos poucos vai se tornando uma memória recuperada. 
+    Em direção ao Leste da floresta, corro, e não paro. Tento não fazer barulhos, e ficar sempre atenta. 
+    Escuto um barulho à minha frente, não consigo ver o que é, mas escuto uma voz cantando. É uma pessoa! O que eu faço?`);
+    setOption1("Me escondo");  
+    setOption2("Converso com a pessoa");
+  }
+
+  function choose11(){
+    setHistory(` Deitada eles entram novamente no quarto, me olham e conversam entre si. 
+    Estranho! O que falam? Não entendo nada. Começo a chorar! Por que eu estou chorando? 
+    Tudo parece tão confuso e um moço vem do meu lado e chora junto, quando eu miro ele outra moça se aproxima e me pede desculpa.`);
+    setOption1("Me deito");
+    setOption2("Pulo a janela");
+  }
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
     return(
         <SafeAreaView style={styles.container} id="container">
           <ScrollView
             contentContainerStyle={styles.scrollView}
             refreshControl={
               <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }
-          >
+            }>
             <ImageBackground source={image} resizeMode="cover" style={styles.image}> 
               <View style={styles.boxText}>
                 <Text style={styles.text}>{history}</Text>
               </View>
-              <Pressable style={styles.button} onPress={choose1()}>
+              <Pressable style={styles.button} onPress={choose1}>
                 <Text style={styles.button_text}>{option1}</Text>
               </Pressable>
-              <Pressable style={styles.button} onPress={choose1()}>
+              <Pressable style={styles.button} onPress={choose2}>
                 <Text style={styles.button_text}>{option2}</Text>
               </Pressable>
             </ImageBackground>
@@ -62,10 +77,8 @@ export default function Game(){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'pink',
       alignItems: 'center',
-      justifyContent: 'space-evenly',
-      height: 1000,
+      justifyContent: 'space-evenly'
     },
     scrollView: {
 
@@ -88,7 +101,8 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      width: 420
+      width: 420,
+      height: 800
     },
     button: {
       alignItems: 'center',
